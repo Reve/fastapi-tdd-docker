06a4f98268b5a3c26c9a974a5627a6f7ae96987c
Reve <gheorghitacristian@mac.com>
2021-07-17 21:30:35 +0300

    Commit #3

--- project/app/api/crud.py
+++ project/app/api/crud.py
@@ -25,3 +25,20 @@ async def get(id: int) -> Optional[dict]:
 async def get_all() -> List:
     summamries = await TextSummary.all().values()
     return summamries
+
+
+async def delete(id: int) -> int:
+    summary = await TextSummary.filter(id=id).first().delete()
+    return summary
+
+
+async def put(id: int, payload: SummaryPayloadSchema) -> Optional[dict]:
+    summary = await TextSummary.filter(id=id).update(
+        url=payload.url, summary=payload.summary
+    )
+
+    if summary:
+        update_summary = await TextSummary.filter(id=id).first().values()
+        return update_summary[0]
+
+    return None
